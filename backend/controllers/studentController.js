c0eb581b8d0bf760ace827d3d5655d75ec82bfbe
Pavith-Mindaka <mindakapavith@gmail.com>
2026-09-05 13:25:29 +0530

    Implement student CRUD APIs

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -18,6 +18,110 @@ const createStudent = async(req, res) => {
     }
 };
 
+
+// Get all students
+const getAllStudents = async (req, res) => {
+    try{
+        const students = await Student.find().sort({ createdAt: -1});
+
+        res.status(200).json({
+            success: false,
+            count: students.length,
+            students
+        });
+
+    }catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+// Get single student
+const getStudentById = async (req, res) => {
+    try {
+        const student = await Student.findById(req.params.id);
+
+        if (!student) {
+            return res.status(404).json({
+                success: false,
+                message: "Student not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            student
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+// Update student
+const updateStudent = async (req, res) => {
+    try {
+        const student = await Student.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true,
+                runValidators: true
+            }
+        );
+
+        if (!student) {
+            return res.status(404).json({
+                success: false,
+                message: "Student not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Student updated successfully",
+            student
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+// Delete student
+const deleteStudent = async (req, res) => {
+    try {
+        const student = await Student.findByIdAndDelete(req.params.id);
+
+        if (!student) {
+            return res.status(404).json({
+                success: false,
+                message: "Student not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Student deleted successfully"
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
 module.exports = {
-    createStudent
+    createStudent,
+    getAllStudents,
+    getStudentById,
+    updateStudent,
+    deleteStudent
 }
--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,12 +1,28 @@
 const express = require("express");
 
 const {
-    createStudent
+    createStudent,
+    getAllStudents,
+    getStudentById,
+    updateStudent,
+    deleteStudent
 } = require("../controllers/studentController");
 
 const router = express.Router();
 
 // Create student
 router.post("/", createStudent);
+
+// Get all students
+router.get("/", getAllStudents);
+
+// Get single student
+router.get("/:id", getStudentById);
+
+// Update student
+router.put("/:id", updateStudent);
+
+// Delete student
+router.delete("/:id", deleteStudent);
 
 module.exports = router;
